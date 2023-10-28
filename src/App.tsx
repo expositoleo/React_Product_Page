a3c7821419a5dd8c19324773dcd6d562ec4f8316
expositoleo <leo.expositomarsollier@gmail.com>
2023-10-28 15:43:03 -0300

    builded

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import Header from "./components/Navbar/Header"
-import Footer from "./components/Footer/Footer"
-import { BrowserRouter as Router } from 'react-router-dom';
+import Header from "./components/Navbar/Header";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
 import { Suspense } from "react";
 import Loader from "./components/Loader/Loader";
 import { Container, ToastContainer } from "react-bootstrap";
 function App() {
-
   return (
     <>
       <ToastContainer />
       <Router>
         <Header />
-        <Container style={{ minHeight: '100vh', minWidth: '100%', padding: '0' }}>
+        <Container
+          style={{ minHeight: "100vh", minWidth: "100%", padding: "0" }}
+        >
           <Suspense fallback={<Loader />}>
             <AppRoutes />
           </Suspense>
@@ -21,7 +22,7 @@ function App() {
         <Footer />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
